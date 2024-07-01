813bd1406ae3d7b25ab8238c85e3461a7dbdba7d
Avery Lor <averywlor@gmail.com>
2024-06-30 22:03:01 -0400

    Added more text.

--- notes.py
+++ notes.py
@@ -1,4 +1,18 @@
-# Linear Regression
+"""Setup and Imports"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compat.v2.feature_column as fc
+
+import tensorflow as tf
+
+"""Linear Regression"""
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,4 +22,20 @@ y = [1, 4, 7, 9, 15]
 plt.plot(x, y, 'ro')
 plt.axis([0, 6, 0, 20])
 
-#
+"""Equation for a line of best fit for this type of graph"""
+
+plt.plot(x, y, 'ro')
+plt.axis([0, 6, 0, 20])
+plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+plt.show()
+
+"""Data"""
+#The following is an example of importing training dating.
+
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+
+#The .head() method from pandas will show the first 5 items in the dataframe. 
+
